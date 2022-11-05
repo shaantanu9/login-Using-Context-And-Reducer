dea6fba449ac6c9dbb9a4c522e7225fae1958ee3
Shantanu Bombatkar <33876955+shaantanu9@users.noreply.github.com>
2022-11-05 21:33:46 +0530

    Initial commit

---
+++ src/context/reducer.js
@@ -0,0 +1,17 @@
+import { LOGIN, LOGOUT } from "./types";
+export default function reducer(state, { type, payload }) {
+  switch (type) {
+    case LOGIN: {
+      return { ...state, token: payload, isAuth: true };
+    }
+    case LOGOUT: {
+      return { ...state, token: null, isAuth: false };
+    }
+    default:
+      return state;
+  }
+}
+
+// dispatch take ? => Action => Object => type and Payload
+
+// dispatch({type:LOGIN, payload:"asdfasdf"})
